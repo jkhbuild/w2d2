835596a66fdd7abfb9b04a120924f70998017702
jkhbuild <jkh.build@gmail.com>
2022-09-20 10:21:51 -0400

    Hangman part 1 completed

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -5,11 +5,28 @@ class Hangman
     random_word
   end
 
+  attr_accessor :guess_word, :attempted_chars, :remaining_incorrect_guesses
+
   def initialize
     @secret_word = Hangman::random_word
     @guess_word = Array.new(@secret_word.length, "_")
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    indices = []
+    @secret_word.each_char.with_index { |ele, i| indices << i if char == ele }
+    indices
+  end 
+
+  def fill_indices (char, indices)
+    indices.each { |i| @guess_word[i] = char }
+  end
+
 
 end
