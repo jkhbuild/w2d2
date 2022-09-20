10e84229c3d1f906cf4d3924746f63ac249a2a07
jkhbuild <jkh.build@gmail.com>
2022-09-20 11:41:00 -0400

    Completed hangman project

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -33,6 +33,10 @@ class Hangman
     if self.already_attempted?(char)
       print "that has already been attempted"
       return false
+    # elsif !(self.already_attempted?(char)) && @secret_word.include?(char)
+    #   @attempted_chars << chars
+    #   self.fill_indices(char, self.get_matching_indices(char))
+    #   @remaining_incorrect_guesses -= 1
     end
 
     @attempted_chars << char
@@ -41,16 +45,37 @@ class Hangman
     @remaining_incorrect_guesses -= 1 if !@secret_word.include?(char)
 
     true
-    # WHY DOESNT THIS WORK?
-    # elsif !self.already_attempted?(char) && @secret_word.include?(char)
-   #    @attempted_chars << chars
-    #   self.fill_indices(char, self.get_matching_indices(char))
-    # else
-    #   @remaining_incorrect_guesses -= 1
+    
   end
 
   def ask_user_for_guess
     print "Enter a char"
     self.try_guess(gets.chomp)
   end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      print "\n" "WIN"
+      return true
+    end
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "\n" "LOSE"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    if self.win? || self.lose?
+      puts @secret_word
+      return true
+    end
+    false
+  end
 end
+
+
