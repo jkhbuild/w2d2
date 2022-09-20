fef80a4facac01db62e1ac67fce83e89bd523221
jkhbuild <jkh.build@gmail.com>
2022-09-20 11:19:31 -0400

    Hangman try guess & ask user for guess methods

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
   def self.random_word
@@ -24,9 +25,32 @@ class Hangman
     indices
   end 
 
-  def fill_indices (char, indices)
+  def fill_indices(char, indices)
     indices.each { |i| @guess_word[i] = char }
   end
 
+  def try_guess(char)
+    if self.already_attempted?(char)
+      print "that has already been attempted"
+      return false
+    end
 
+    @attempted_chars << char
+    self.fill_indices(char, self.get_matching_indices(char))
+
+    @remaining_incorrect_guesses -= 1 if !@secret_word.include?(char)
+
+    true
+    # WHY DOESNT THIS WORK?
+    # elsif !self.already_attempted?(char) && @secret_word.include?(char)
+   #    @attempted_chars << chars
+    #   self.fill_indices(char, self.get_matching_indices(char))
+    # else
+    #   @remaining_incorrect_guesses -= 1
+  end
+
+  def ask_user_for_guess
+    print "Enter a char"
+    self.try_guess(gets.chomp)
+  end
 end
