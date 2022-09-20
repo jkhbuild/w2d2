95c52a15bc5ff6d130d942a1229f73c5d9c0a0f5
jkhbuild <jkh.build@gmail.com>
2022-09-20 15:29:14 -0400

    Startup project part 2

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,45 @@
 require "employee"
 
 class Startup
+    attr_reader :name, :funding, :salaries, :employees
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    def valid_title?(title)
+        @salaries.has_key?(title)
+    end
+
+    def >(instance)
+        self.funding > instance.funding
+    end
+
+    def hire(employee_name, title)
+        if self.valid_title?(title)
+            @employees << Employee.new(employee_name, title)
+        else
+            raise "not valid title"
+        end
+    end
+
+    def size
+        @employees.length
+    end
+
+    def pay_employee(employee)
+        if @funding > @salaries[employee.title]
+            employee.pay(@salaries[employee.title])
+            @funding -= @salaries[employee.title]
+        else
+            raise "not enough funding"
+        end
+    end
+
+    def payday
+        @employees.each { |employee| pay_employee(employee) }
+    end
 
 end
