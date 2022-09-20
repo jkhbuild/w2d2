3bba67077b9c5333caaf4b44169ed39605a683f0
jkhbuild <jkh.build@gmail.com>
2022-09-20 11:57:08 -0400

    Hotel project part 1

--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
+    attr_accessor :capacity, :occupants
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+    def full?
+        return false if @capacity > @occupants.length
+        true
+    end
+
+    def available_space
+        space_available = @capacity - @occupants.length
+        space_available
+    end
+
+    def add_occupant(name)
+        if !(self.full?)
+            @occupants << name
+            return true
+        end
+        false
+    end
+
   
 end
